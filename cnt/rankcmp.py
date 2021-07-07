effb4d98fd71d2bfc4b7b24b52784b401d3d89cd
Chuanwang WANG <chaunecy@outlook.com>
2021-07-07 15:44:51 +0800

    feat(nargs): read several files each time

--- cnt/rankcmp.py
+++ cnt/rankcmp.py
@@ -11,34 +11,42 @@ from collections import defaultdict
 from typing import TextIO, List, Dict, Tuple
 
 
-def read_raw_data(fd: TextIO, skip=1, splitter=re.compile("\t"), idx_pwd=0, idx_rank=1, idx_num=2):
-    lst = []
-    for _ in range(skip):
-        fd.readline()
-    for line in fd:
-        line = line.strip("\r\n")
-        items = splitter.split(line)
-        pwd = items[idx_pwd]
-        rank = float(items[idx_rank])
-        num = int(items[idx_num])
-        lst.append((pwd, rank, num))
-    fd.close()
-    lst = sorted(lst, key=lambda x: x[1])
-    _map = {pwd: (rank, freq) for pwd, rank, freq in lst}
-    return _map
+def read_raw_data(fds: List[TextIO], skip=1, splitter=re.compile("\t"), idx_pwd=0, idx_rank=1, idx_num=2):
+    maps = []
+    for fd in fds:
+        lst = []
+        for _ in range(skip):
+            fd.readline()
+        for line in fd:
+            line = line.strip("\r\n")
+            items = splitter.split(line)
+            pwd = items[idx_pwd]
+            rank = float(items[idx_rank])
+            num = int(items[idx_num])
+            lst.append((pwd, rank, num))
+        fd.close()
+        lst = sorted(lst, key=lambda x: x[1])
+        _map = {pwd: (rank, freq) for pwd, rank, freq in lst}
+        maps.append(_map)
+    return maps
 
 
 def gen_table(guess_number_thresholds: List[int], guess_number_display_list: List[str],
-              map_a: Dict[str, Tuple[int, int]], map_b: Dict[str, Tuple[int, int]]):
-    table = defaultdict(lambda: defaultdict(lambda: 0))
-    for pwd, info_a in map_a.items():
-        rank_a, cnt = info_a
-        # info_b = map_b.get(pwd, (sys.maxsize, info_a[1]))
-        rank_b = map_b.get(pwd, (sys.maxsize, cnt))[0]
-        idx_a = bisect.bisect_left(guess_number_thresholds, rank_a)
-        idx_b = bisect.bisect_left(guess_number_thresholds, rank_b)
-        table[idx_b][idx_a] += cnt
-    total = sum([f for _, f in map_a.values()])
+              map_as: List[Dict[str, Tuple[int, int]]], map_bs: List[Dict[str, Tuple[int, int]]]):
+    tables = []
+    totals = []
+    for index in range(len(map_as)):
+        map_a = map_as[index]
+        map_b = map_bs[index]
+        tables.append(defaultdict(lambda: defaultdict(lambda: 0)))
+        for pwd, info_a in map_a.items():
+            rank_a, cnt = info_a
+            # info_b = map_b.get(pwd, (sys.maxsize, info_a[1]))
+            rank_b = map_b.get(pwd, (sys.maxsize, cnt))[0]
+            idx_a = bisect.bisect_left(guess_number_thresholds, rank_a)
+            idx_b = bisect.bisect_left(guess_number_thresholds, rank_b)
+            tables[index][idx_b][idx_a] += cnt
+        totals.append(sum([f for _, f in map_a.values()]))
 
     def print_table(splitter="\t", percent=True):
         for i in range(1, len(guess_number_thresholds) + 1):
@@ -49,9 +57,15 @@ def gen_table(guess_number_thresholds: List[int], guess_number_display_list: Lis
                 else:
                     the_end = splitter
                 if percent:
-                    print(f"{table[i][j] / total * 100:5.2f}%", end=the_end)
+                    val = 0
+                    for index in range(len(tables)):
+                        val += tables[index][i][j] / totals[index]
+                    print(f"{val / len(tables) * 100:5.2f}%", end=the_end)
                 else:
-                    print(f"{table[i][j]:10,d}", end=the_end)
+                    val = 0
+                    for index in range(len(tables)):
+                        val += tables[index][i][j]
+                    print(f"{val / len(tables):5.0f}", end=the_end)
 
     print("Print percentages, LaTeX format.\n"
           "file a is placed on the top of the table, file b is placed on the left of the table.")
@@ -63,9 +77,9 @@ def gen_table(guess_number_thresholds: List[int], guess_number_display_list: Lis
 
 def wrapper():
     cli = argparse.ArgumentParser("Unsafe Errors")
-    cli.add_argument("-a", "--file-a", dest="fd_a", required=True, type=argparse.FileType('r'),
+    cli.add_argument("-a", "--file-a", dest="fd_a", required=True, type=argparse.FileType('r'), nargs='+',
                      help="one file containing passwords, ranks and frequencies")
-    cli.add_argument("-b", "--file-b", dest="fd_b", required=True, type=argparse.FileType('r'),
+    cli.add_argument("-b", "--file-b", dest="fd_b", required=True, type=argparse.FileType('r'), nargs='+',
                      help="another file containing passwords, ranks and frequencies")
     cli.add_argument("--idx-pwd", dest="idx_pwd", required=True, type=int,
                      help="passwords are in \"idx-pwd\"th column, start from 0")
