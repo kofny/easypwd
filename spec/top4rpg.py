cc0b3c9fa43c71d43cc8dfde3e804e5459b0c641
Chuanwang WANG <chaunecy@outlook.com>
2022-02-17 14:19:57 +0800

    feat(info)

--- spec/top4rpg.py
+++ spec/top4rpg.py
@@ -18,6 +18,7 @@ def read_rules(rule_path) -> List[str]:
 def top_rules(log_path: str, rules: List[str], n: int):
     counter = [[i, 0] for i in range(len(rules))]
     with open(log_path, 'r') as f_log:
+        parsed = 0
         for line in f_log:
             if not line.startswith('['):
                 continue
@@ -25,6 +26,9 @@ def top_rules(log_path: str, rules: List[str], n: int):
             word, rule_ids = json.loads(line)
             for rule_id in rule_ids:
                 counter[rule_id][1] += 1
+            parsed += 1
+            if parsed % 1000 == 0:
+                print(f"parsed {parsed}\r")
     sorted(counter, key=lambda x: x[1], reverse=True)
     n = max(min(len(rules), n), 1)
     wanted_rules = [[rules[rule_id], count] for rule_id, count in counter[:n]]
@@ -62,13 +66,15 @@ def wrapper():
     cli.add_argument('-r', '--rules', dest='rules_path', required=True, type=str, help='filepath of rules')
     cli.add_argument('--log', dest='log_path', type=str, help='filepath of log')
     cli.add_argument('--hit', dest='hit_path', type=str, help='filepath of hit passwords based on `words + rules`')
-    cli.add_argument('-n', '--top-n', dest='n', required=False, type=int, help='top n rules to display')
+    cli.add_argument('-n', '--top-n', dest='n', type=int, default=10, help='top n rules to display')
     args = cli.parse_args()
     rules = read_rules(rule_path=args.rules_path)
-    chosen_rules, total_chosen = top_rules(log_path=args.log_path, rules=rules, n=args.n)
-    hit_rules, total_hit = top_hit_rules(hit_path=args.hit_path, rules=rules, n=args.n)
-    printing(chosen_rules, total_chosen, msg='Chosen rules by model')
-    printing(hit_rules, total_hit, msg='Hit rules by `words + rules`')
+    if args.log_path is not None:
+        chosen_rules, total_chosen = top_rules(log_path=args.log_path, rules=rules, n=args.n)
+        printing(chosen_rules, total_chosen, msg='Chosen rules by model')
+    if args.hit_path is not None:
+        hit_rules, total_hit = top_hit_rules(hit_path=args.hit_path, rules=rules, n=args.n)
+        printing(hit_rules, total_hit, msg='Hit rules by `words + rules`')
     pass
 
 
